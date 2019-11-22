6404a03c23b3c335810f401be3aeb15b4508d5f7
Séy' A. Cileli <52051065+HanSupreme@users.noreply.github.com>
2019-11-21 20:12:40 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package November19BankAccount;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	static Functions functions = new Functions();
+	static MainMenu MainMenu = new MainMenu();
+	
+	static Scanner sc;
+
+	public static void main(String[] args) {
+		
+		sc = new Scanner(System.in);
+
+		sc.close();		
+	}
+	
+}
---
+++ UpdateAccountInfo.java
@@ -0,0 +1,12 @@
+package November19BankAccount;
+
+public class UpdateAccountInfo {
+
+	static Functions functions = new Functions();
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
